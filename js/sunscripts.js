8f32394fde335575a5c1babff9df7557866ae12d
stevenhe24 <wjhe@bu.edu>
2024-04-24 20:52:17 -0400

    Updates

---
+++ js/sunscripts.js
@@ -0,0 +1,49 @@
+// Define image array
+let images = [
+    { img: 'Sun-1.png', name: 'Oracle Bone<br> 1600 to 1100 BC' },
+    { img: 'Sun-2.png', name: 'Bronze<br> 1000 BC to 200 BC' },
+    { img: 'Sun-3.png', name: 'Small Seal<br> 221 to 209 BCE' },
+    { img: 'Sun-4.png', name: 'Clerical<br> 206 BCE to 220 CE' },
+    { img: 'Sun-5.png', name: 'Regular<br> 200 CE to Present' },
+    { img: 'Sun-6.png', name: 'Simplified<br> 1956 to Present' }]
+// Define current avlue
+let currentIndex = 0
+  
+// Get Left / Right Elements
+  let prevBtn = document.querySelector(".prev-btn")
+  let nextBtn = document.querySelector(".next-btn")
+  let showList = document.querySelector(".show-list")
+  let currentTip = document.querySelector(".sun-tip")
+  let currentName = document.querySelector(".sun-name")
+  let currentImage = document.querySelector(".sun-img")
+  
+  window.onload = function () {
+    createSun()
+}
+  
+// Previous button click event
+  prevBtn.addEventListener("click", function () {
+    currentIndex = (currentIndex - 1 + images.length) % images.length
+    createSun()
+})
+  
+// Next button click event
+  nextBtn.addEventListener("click", function () {
+    currentIndex = (currentIndex + 1) % images.length
+    createSun()
+})
+  
+function createSun () {
+    currentName.innerHTML = images[currentIndex].name
+    currentImage.src = `images/${images[currentIndex].img}`
+    if (currentIndex == 0) {
+      showList.classList.add("active")
+      currentTip.style.display = "block"
+      prevBtn.style.display = "none"
+    } else {
+      showList.classList.remove("active")
+      currentTip.style.display = "none"
+      prevBtn.style.display = "block"
+    }
+    nextBtn.style.display = currentIndex == images.length - 1 ? "none" : "block"
+}
---
+++ js/waterscripts.js
@@ -0,0 +1,50 @@
+// Define image array
+let images = [
+    { img: 'water-1.png', name: 'Oracle Bone<br> 1600 to 1100 BC' },
+    { img: 'water-2.png', name: 'Bronze<br> 1000 BC to 200 BC' },
+    { img: 'water-3.png', name: 'Small Seal<br> 221 to 209 BCE' },
+    { img: 'water-4.png', name: 'Clerical<br> 206 BCE to 220 CE' },
+    { img: 'water-5.png', name: 'Regular<br> 200 CE to Present' },
+    { img: 'water-6.png', name: 'Simplified<br> 1956 to Present' }]
+// Define current avlue
+let currentIndex = 0
+  
+// Get Left / Right Elements
+  let prevBtn = document.querySelector(".prev-btn")
+  let nextBtn = document.querySelector(".next-btn")
+  let showList = document.querySelector(".show-list")
+  let currentTip = document.querySelector(".water-tip")
+  let currentName = document.querySelector(".water-name")
+  let currentImage = document.querySelector(".water-img")
+  
+  window.onload = function () {
+    createWater()
+}
+  
+// Previous button click event
+  prevBtn.addEventListener("click", function () {
+    currentIndex = (currentIndex - 1 + images.length) % images.length
+    createWater()
+})
+  
+// Next button click event
+  nextBtn.addEventListener("click", function () {
+    currentIndex = (currentIndex + 1) % images.length
+    createWater()
+})
+  
+function createWater () {
+    currentName.innerHTML = images[currentIndex].name
+    currentImage.src = `images/${images[currentIndex].img}`
+    if (currentIndex == 0) {
+      showList.classList.add("active")
+      currentTip.style.display = "block"
+      prevBtn.style.display = "none"
+    } else {
+      showList.classList.remove("active")
+      currentTip.style.display = "none"
+      prevBtn.style.display = "block"
+    }
+    nextBtn.style.display = currentIndex == images.length - 1 ? "none" : "block"
+}
+
